1e2cbc319d1bf14957a68c6c0480903d0b4aa96c
Matthew.McDade13 <Matthew.McDade13@gmail.com>
2019-12-04 11:13:43 -0800

    update gitignore

--- Graphics/Camera.h
+++ Graphics/Camera.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef PURE2D_CAMERA_H
 #define PURE2D_CAMERA_H
 
@@ -33,39 +32,3 @@ namespace pure
 
 
 #endif // PURE2D_CAMERA_H
-=======
-#ifndef PURE2D_CAMERA_H
-#define PURE2D_CAMERA_H
-
-#include <Pure2D/Define.h>
-#include <Pure2D/Math/Vec3.h>
-
-namespace pure
-{
-	struct Mat4;
-
-	struct Camera
-	{
-		Vec3f position;
-		Vec3f lookDir;
-		Vec3f rotation;
-
-		float speed;
-		float sensitivity;
-
-		PURE2D_API Mat4 view() const;
-		PURE2D_API Vec3f target() const;
-
-		PURE2D_API void moveForward(float dt = 1.f);
-		PURE2D_API void moveBackward(float dt = 1.f);
-		PURE2D_API void moveLeft(float dt = 1.f);
-		PURE2D_API void moveRight(float dt = 1.f);
-		PURE2D_API void lookTowards(const Vec3f& offset);
-
-	};
-
-}
-
-
-#endif // PURE2D_CAMERA_H
->>>>>>> 122077b860bcf9e03fd9e59de88d008efff1773b
